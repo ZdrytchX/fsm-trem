d02ddf69201a4806cff5c40018bd61069ddcdbaf
SlackerLinux85 <SlackerLinux85@ba17b805-6f52-0410-b2ac-cdbc6f944c2b>
2008-07-30 03:49:32 +0000

    cURL fixes (Serverside) (Issue 43)

--- src/server/sv_init.c
+++ src/server/sv_init.c
@@ -629,8 +629,8 @@ void SV_Init (void) {
 	sv_allowDownload = Cvar_Get ("sv_allowDownload", "0", CVAR_SERVERINFO);
 	Cvar_Get ("sv_dlURL", "", CVAR_SERVERINFO | CVAR_ARCHIVE);
 	//These two are only for compatibility with old clients
-	Cvar_Get ("sv_wwwBaseURL", "", CVAR_SERVERINFO | CVAR_ARCHIVE);
-	Cvar_Get ("sv_wwwDownload", "1", CVAR_SERVERINFO | CVAR_ARCHIVE);
+	Cvar_Get ("sv_wwwBaseURL", "", CVAR_SYSTEMINFO | CVAR_ARCHIVE);
+	Cvar_Get ("sv_wwwDownload", "1", CVAR_SYSTEMINFO | CVAR_ARCHIVE);
 	sv_master[0] = Cvar_Get ("sv_master1", MASTER_SERVER_NAME, 0 );
 	sv_master[1] = Cvar_Get ("sv_master2", "", CVAR_ARCHIVE );
 	sv_master[2] = Cvar_Get ("sv_master3", "", CVAR_ARCHIVE );
