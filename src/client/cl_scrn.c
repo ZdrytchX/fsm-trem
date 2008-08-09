6425a41bd051128bd0ad0e361d868e6ae94c177a
SlackerLinux85 <SlackerLinux85@ba17b805-6f52-0410-b2ac-cdbc6f944c2b>
2008-08-09 02:35:34 +0000

    Googles voip display cleanup (Issue 63)

--- src/client/cl_scrn.c
+++ src/client/cl_scrn.c
@@ -382,10 +382,10 @@ void SCR_DrawVoipSender( void ) {
 	else if (!cl_voip->integer)
 		return;  // client has VoIP support disabled.
 
-	sprintf( string, "Client speaking: %s ^7(%i)", cls.voipSenderStr, cls.voipSender );
+	sprintf( string, "Client speaking: %s^7 (%i)", cls.voipSenderStr, cls.voipSender );
 	
-	// Display below the VoIP meter
-	SCR_DrawStringExt( 320 - strlen( string ) * 4, 30, 9, string, g_color_table[7], qfalse, qfalse );
+	// I hardcoded the display to be on the left side of the screen, and not to move
+	SCR_DrawStringExt( 6, 310, 12, string, g_color_table[7], qfalse, qfalse );
        }
 }
 #endif
