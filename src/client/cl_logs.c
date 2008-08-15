9a1dfe23bbda44c5adbb26deee7c0cb0981540fb
SlackerLinux85 <SlackerLinux85@ba17b805-6f52-0410-b2ac-cdbc6f944c2b>
2008-08-15 00:40:25 +0000

    Reverted the tremfusion changes (Breaks the log file newlines) Added map info to logs (Issue 75)

--- src/client/cl_logs.c
+++ src/client/cl_logs.c
@@ -62,8 +62,7 @@ void CL_WriteClientLog( char *text ) {
 			// varibles
 			char NoColorMsg[MAXPRINTMSG];
 			//decolor the string
-			Q_strncpyz( NoColorMsg, text, sizeof(NoColorMsg) );
-			Q_CleanStr( NoColorMsg );
+			Com_DecolorString( text, NoColorMsg, sizeof(NoColorMsg) );
 			//write to the file
 			FS_Write( NoColorMsg, strlen( NoColorMsg ), LogFileHandle );
 			//flush the file so we can see the data
@@ -111,8 +110,7 @@ void CL_WriteClientChatLog( char *text ) {
 					sprintf( Timestamp, "[%d:%02d][%d:%02d]", now.tm_hour, now.tm_min, cl.serverTime / 60000, ( cl.serverTime / 1000 ) % 60 ); //server Time
 				}
 				//decolor the string
-				Q_strncpyz( NoColorMsg, text, sizeof(NoColorMsg) );
-				Q_CleanStr( NoColorMsg );
+				Com_DecolorString( text, NoColorMsg, sizeof(NoColorMsg) );
 				//prepare text for log
 				sprintf( LogText, "%s%s", Timestamp, NoColorMsg ); //thing to write to log
 				//write to the file
